c0d3fc35f802e2ac3804af01575a4f1ac6dcc4fe
Piotr Gazda <pjgazda@interia.pl>
2024-05-12 13:41:13 +0200

    Add radix sort implementation.

--- Algorithms/src/com/gazda/algorithms/Launcher.java
+++ Algorithms/src/com/gazda/algorithms/Launcher.java
@@ -1,6 +1,7 @@
 package com.gazda.algorithms;
 
 import com.gazda.algorithms.sorting.CountingSort;
+import com.gazda.algorithms.sorting.RadixSort;
 
 import java.util.Arrays;
 
@@ -9,6 +10,11 @@ public class Launcher {
         CountingSort countingSort = new CountingSort();
         int[] arrayToSort = new int[] {2,5,3,0,2,3,0,3};
         int[] sortedArray = countingSort.countingSort(arrayToSort);
+        Arrays.stream(sortedArray).forEach(System.out::print);
+
+        RadixSort radixSort = new RadixSort();
+        arrayToSort = new int[] {175, 45, 75, 90, 802, 24, 2, 66};
+        sortedArray = radixSort.radixSort(arrayToSort);
         Arrays.stream(sortedArray).forEach(System.out::print);
     }
 }
